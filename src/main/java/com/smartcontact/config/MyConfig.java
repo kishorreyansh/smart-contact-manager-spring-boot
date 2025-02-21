bfc7a74445885dc72b6080546923909a2b60c50e
kishorreyansh <kishor.andekar2389@gmail.com>
2025-02-20 22:55:31 -0600

    spring security implemented

--- src/main/java/com/smartcontact/config/MyConfig.java
+++ src/main/java/com/smartcontact/config/MyConfig.java
@@ -1,8 +1,64 @@
 package com.smartcontact.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class MyConfig {
+
+	@Bean
+	public UserDetailsService getUserDetailService() {
+		return new UserDetailsServiceImpl();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+		daoAuthenticationProvider.setUserDetailsService(this.getUserDetailService());
+
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+		return daoAuthenticationProvider;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(csrf -> csrf.disable()) // Disable CSRF for testing (Enable in production)
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/admin/**").hasRole("ADMIN")
+						.requestMatchers("/user/**").hasRole("USER")
+						.requestMatchers("/**").permitAll())
+				// .formLogin(form -> form.permitAll())
+				.formLogin(form -> form
+						.loginPage("/signin")
+						.loginProcessingUrl("/dologin")
+						.defaultSuccessUrl("/user/index")
+						// .failureUrl("login-fail")
+						.permitAll())
+				.logout(logout -> logout.permitAll());
+
+		return http.build();
+	}
 
 }
--- src/main/java/com/smartcontact/controller/HomeController.java
+++ src/main/java/com/smartcontact/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.smartcontact.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,6 +23,9 @@ public class HomeController {
 	@Autowired
 	private UserRepository userRespository;
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
 	@RequestMapping("/")
 	public String home(Model model) {
 		model.addAttribute("title", "Home - Smart Contact Manager");
@@ -34,8 +38,14 @@ public class HomeController {
 		return "about";
 	}
 
+	@RequestMapping("/signin")
+	public String customLogin(Model model) {
+		model.addAttribute("title", "Login Page");
+		return "login";
+	}
+
 	@RequestMapping("/signup")
-	public String signup(Model model,HttpSession session) {
+	public String signup(Model model, HttpSession session) {
 		model.addAttribute("title", "Register - Smart Contact Manager");
 		model.addAttribute("user", new User());
 		session.removeAttribute("message");
@@ -44,34 +54,36 @@ public class HomeController {
 
 	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
 	public String register(@Valid @ModelAttribute("user") User user, BindingResult bindingResult,
-			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session) {
-		try{
-			if(!agreement){
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
+			HttpSession session) {
+		try {
+			if (!agreement) {
 				System.out.println("YOU HAVE NOT AGREED THE TERMS AND CONDITION");
 				throw new Exception("YOU HAVE NOT AGREED THE TERMS AND CONDITION");
 			}
-			if(bindingResult.hasErrors()) {
-				System.out.println("ERROR "+bindingResult.toString());
+			if (bindingResult.hasErrors()) {
+				System.out.println("ERROR " + bindingResult.toString());
 				model.addAttribute("user", user);
 				return "signup";
 			}
 			user.setRole("ROLE_USER");
 			user.setEnabled(true);
 			user.setImageUrl("abcd.png");
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
 			System.out.println("Agreement " + agreement);
 			System.out.println("User " + user);
-	
-			//saving in DB
+
+			// saving in DB
 			User result = this.userRespository.save(user);
 			model.addAttribute("user", new User());
 			session.setAttribute("message", new Message("Successfully Registered !!", "alert-success"));
 			return "signup";
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			model.addAttribute("user", user);
-			session.setAttribute("message", new Message("Something went wrong !! "+e.getMessage(), "alert-danger"));
+			session.setAttribute("message", new Message("Something went wrong !! " + e.getMessage(), "alert-danger"));
 			return "signup";
 		}
-		
+
 	}
 }
