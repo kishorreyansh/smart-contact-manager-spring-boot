c2d5acc5e6496c94e4b051ba356d375055bda344
kishorreyansh <kishor.andekar2389@gmail.com>
2025-02-27 22:56:31 -0600

    Created Add Contact Form and saving data in db, uploading image in db

--- src/main/java/com/smartcontact/controller/UserController.java
+++ src/main/java/com/smartcontact/controller/UserController.java
@@ -1,15 +1,91 @@
 package com.smartcontact.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.smartcontact.entities.Contact;
+import com.smartcontact.entities.User;
+import com.smartcontact.helper.Message;
+import com.smartcontact.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    // Method for adding common data for response
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
+        String userName = principal.getName();
+        System.out.println("User Name is :" + userName);
+        User user = userRepository.getUserByUserName(userName);
+        System.out.println("User Details: " + user);
+        model.addAttribute("user", user);
+    }
+
+    // Dashboard Home
     @RequestMapping("/index")
-    public String dashboard() {
+    public String dashboard(Model model, Principal principal) {
+        model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
+    }
+
+    // Open Add Contact Handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
+    }
+
+    @PostMapping("/process-contact")
+    public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file,
+            Principal principal, HttpSession session) {
+        try {
+            String name = principal.getName();
+            User user = this.userRepository.getUserByUserName(name);
+
+            if (file.isEmpty()) {
+                System.out.println("File Cannot be Empty");
+            } else {
+                contact.setImage(file.getOriginalFilename());
+                File saveFile = new ClassPathResource("/static/images").getFile();
+                Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Image is Uploaded successfully !!!");
+            }
+
+            user.getContacts().add(contact);
+            contact.setUser(user);
+            this.userRepository.save(user);
+            System.out.println("Contact:     " + contact);
+            session.setAttribute("message", new Message("Your Contact Added !!", "success"));
+        } catch (Exception e) {
+            System.out.println("Error Message: " + e.getMessage());
+            e.printStackTrace();
+            session.setAttribute("message", new Message("Something went wrong !!", "danger"));
+        }
+
+        return "normal/add_contact_form";
     }
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,11 @@
-console.log("Script File");
+const toggleSidebar = () => {
+    let sidebar = document.querySelector('.sidebar');
+    let content = document.querySelector('.content');
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        content.style.marginLeft = "0";
+    } else {
+        sidebar.style.width = "250px";
+        content.style.marginLeft = "260px";
+    }
+}
